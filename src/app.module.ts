1848ef217254535d748bb65c07c100e8a5c62d3f
Nathan Freeman <NathanLFreeman@gmail.com>
2022-06-30 23:57:35 -0400

    Added .env and support for .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ProductsModule, 
     MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
