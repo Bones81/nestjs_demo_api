315633168db999f020a37c89e3693dbbccad741a
Nathan Freeman <NathanLFreeman@gmail.com>
2022-06-30 21:15:26 -0400

    Setup; Get all, Get One, and Create routes made

---
+++ src/products/products.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Product } from './product.model'
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+
+  insertProduct(title: string, desc: string, price: number) {
+    const prodId = Math.random().toString();
+    const newProduct = new Product(prodId, title, desc, price);
+    this.products.push(newProduct)
+    return prodId
+  }
+
+  getProducts() {
+    return [...this.products]; //creates copy which safeguards against messing with original products array from outside of service
+  }
+
+  getSingleProduct(productId: string) {
+    const product = this.products.find((prod) => prod.id === productId);
+    if (!product) {
+      throw new NotFoundException('Could not find product.');
+    }
+    return { ...product };
+  }
+}
