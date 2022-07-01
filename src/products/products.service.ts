2d013444c0ce129c02c604f909b24c8e0b78b2a7
Nathan Freeman <NathanLFreeman@gmail.com>
2022-06-30 22:35:57 -0400

    Atlas and Mongoose setup started; Create done

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,24 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+
 import { Product } from './product.model'
+import { Model } from 'mongoose'
+
 @Injectable()
 export class ProductsService {
   private products: Product[] = [];
 
-  insertProduct(title: string, desc: string, price: number) {
-    const prodId = Math.random().toString();
-    const newProduct = new Product(prodId, title, desc, price);
-    this.products.push(newProduct)
-    return prodId
+  constructor(@InjectModel('Product') private readonly productModel: Model<Product>) {}
+
+  async insertProduct(title: string, desc: string, price: number) {
+    const newProduct = new this.productModel({
+      title: title, 
+      description: desc, 
+      price: price
+    });
+    const result = await newProduct.save()
+    console.log(result)
+    return result.id as string;
   }
 
   getProducts() {
